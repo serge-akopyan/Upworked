7c3cda3c5dfa916c4ad1f318afcadea31fdf621e
serge-akopyan <realsergeakopyan@gmail.com>
2022-06-26 21:22:54 +0300

    .

--- inputs.js
+++ inputs.js
@@ -1,18 +1,10 @@
-var addid = 0;
-
-function addInput(id) {
-    
-    var addList = document.getElementById('addlist');
-    var docstyle = addList.style.display;
-    if (docstyle == 'none') addList.style.display = '';
-
-    addid++;
-    
-    var text = document.createElement('div');
-    text.id = 'additem_' + addid;
-    text.innerHTML = "<input type='text' value='' class='buckinput' name='items[]' style='padding:5px;' /> <a href='javascript:void(0);' onclick='addInput(" + addid + ")' id='addlink_" + addid + "'>add more</a>";
-
-    addList.appendChild(text);
+var counter = 1;
+function addInput(divName){
+var input = document.createElement('input');
+          //newdiv.innerHTML = '<div><input type="text></div>';
+          input.type = 'text';
+          //input.setAttribute('type', 'text');
+          input.className = 'keyword';
+          document.getElementById(divName).appendChild(input);
+          counter++;
 }
-
-addInput(1);
